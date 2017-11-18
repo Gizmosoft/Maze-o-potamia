4e7aa9595576be11eb067902e5c9f484e0a4bc25
Gizmosoft <Kartikey.hebbar@gmail.com>
2017-11-18 17:55:23 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,36 @@
+print "                    *   *   *   **/  ****     ***     ***  ***  ***   *   *   * ***   *    "
+print "                    * * * * * *  /    **  -- *   * -- * * *   *  *  * * * * * *  *  * * *  "
+print "                    *   * *   * **** ****     ***     *    ***   *  *   * *   * *** *   *  " 
+print "                                   Copyright Gizmosoft Incorporation                       "                                                                                              
+ 
+
+print "Welcome!"
+name=raw_input("Enter your name:")
+print "Hey, Welcome %r, I am Blambert Deinstein. I'll be your companion for the rest of your journey through this game...\n So, what was your name..??? ...%r right? \n Sorry as you can see I am very old, my memory has gone weak." %(name,name)
+print "So tell me %r,"%name
+age=raw_input("What is your age?:")
+if age < 18 :
+	print "Aww... You are so young to be here dear..."
+else:
+	print "Hmmmm... Hope you can handle the situations here."
+print "But anyway, let's continue. As there is no way back for you."
+
+print "So %r, let me tell you a story. A story of the place you are in now."%name
+
+print "This place is called, Mazopotamia. Pronounced as Maze-o-potamia.\nNo one knows about the time when this was formed. But, long ago an incident made this place as it is now. A witch named Octaperri cursed the land of Mazopotamia and converted it from a happy land to a badland as it seems now."
+
+print "Since then, people who came here have got stuck in this place and never made to the exit safely.\nSo you have no other option other than entering this gate and trying out your luck."
+
+print "And yeah, this curse will be reverted as soon as someone makes it to the exit safely.\nSo the responsibility of your own life and the future of Mazopotamia lies in your hand my child. Don't worry I will be there along to guide you.\nAll The Best......"
+ 
+print '***As soon as you enter the door, the whole environment is filled with mist and a shady pulse of wave takes charge of the surrounding. You get scared of the spooky things around when suddenly an anonymous voice is heard. "Welcome to die... Hahahaha. Let\'s see how far do you go. Just answer the questions which follow during each step of your walk in this land."***'
+
+decision=raw_input("What would you do?  Would you go ahead or would you commit suicide? Answer with 'y' or 'n'")
+if decision=='y':
+	print "Okay let's see what you will have to face next."
+else:
+	print "You chose to die! Sorry, in my world there is no place for losers..."
+	print "Hope you enjoyed your journey. Thanks for playing."
+
+
+
